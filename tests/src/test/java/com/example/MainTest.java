b872d0d9521f0a21487c2c32a05570b2c0fc5e70
KrutaRyba <n.u.leshchova@gmail.com>
2024-11-30 16:41:15 +0100

    Dodanie nowych testow

--- tests/src/test/java/com/example/MainTest.java
+++ tests/src/test/java/com/example/MainTest.java
@@ -1,79 +1,74 @@
 package com.example;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 @SuppressWarnings("UnnecessaryReturnStatement")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MainTest {
-    //private static ChromeDriverService driverService;
+    private static ChromeDriverService driverService;
     private static WebDriver webDriver;
     private static WebDriverWait wait;
-
-/*
+    private final Keys controlKey = System.getProperty("os.name").contains("Mac OS") ? Keys.COMMAND : Keys.CONTROL;
     @BeforeAll
     public static void createService() throws IOException {
+        //xattr -d com.apple.quarantine chromedriver
         driverService = new ChromeDriverService.Builder().usingDriverExecutable(new File("chromedriver")).usingAnyFreePort().build();
         driverService.start();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        //ChromeDriver does not support Chrome v131
+        //webDriver = new ChromeDriver(chromeOptions);
+        webDriver = new RemoteWebDriver(driverService.getUrl(), new ChromeOptions());
+        wait = new WebDriverWait(webDriver, 5);
         return;
     }
     @AfterAll   
     public static void stopService() {
-        driverService.stop();
-        return;
-    }
-*/
- 
-    @BeforeEach
-    public void createDriver() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        webDriver = new ChromeDriver(chromeOptions);
-        //webDriver = RemoteWebDriver(driverService.getUrl(), new ChromeOptions());
-        wait = new WebDriverWait(webDriver, 5);
-        return;
-    }
-    @AfterEach
-    public void quitDriver() {
         webDriver.close();
         webDriver.quit();
+        driverService.stop();
         return;
     }
     private int openProductsTabs(String category) {
         webDriver.get(category);
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product-thumbnail")));
         WebElement productList = webDriver.findElement(By.id("js-product-list"));
         int productListSize = productList.findElements(By.className("product-thumbnail")).size();
         int size = Math.min(productListSize, 10);
         for (int i = 0; i < size; i++) {
-            //TODO Keys.COMMAND -> Keys.CONTROL for Windows or Linux
-            String newTabShortcut = Keys.chord(Keys.COMMAND, Keys.ENTER);
             wait.until(ExpectedConditions.elementToBeClickable(By.className("product-thumbnail")));
-            productList.findElements(By.className("product-thumbnail")).get(i).sendKeys(newTabShortcut);
+            productList.findElements(By.className("product-thumbnail")).get(i).sendKeys(Keys.chord(this.controlKey, Keys.ENTER));
         }
         return size;
     }
+    @Order(1)
     @Test
     //TODO change categories
     public void testAddToCart() {
@@ -84,31 +79,25 @@ public class MainTest {
         int[] quantities = IntStream.rangeClosed(1, 20).toArray();
         for (int i = 0, q = 0; i < expectedResult + 2; i++, q++) {
             webDriver.switchTo().window((String) windowHandles[i]);
-            Wait<WebDriver> shortWait = new FluentWait<>(webDriver).withTimeout(2, TimeUnit.SECONDS).pollingEvery(250, TimeUnit.MILLISECONDS);
             try {
-                shortWait.until((WebDriver input) -> {
-                    try {
-                        input.findElement(By.id("quantity_wanted"));
-                        return true;
-                    } catch (NoSuchElementException e) {
-                        return false;
-                    }
-                });
-            } catch (TimeoutException e) {
+                webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+                webDriver.findElement(By.id("quantity_wanted"));
+            } catch (NoSuchElementException e) {
                 q--;
+                if (i < expectedResult + 1) webDriver.close();
                 continue;
             }
-            wait.until(ExpectedConditions.elementToBeClickable(By.className("bootstrap-touchspin-up")));
-            WebElement upButton = webDriver.findElement(By.className("bootstrap-touchspin-up"));
-            for (int j = 1; j < quantities[q]; j++) {
-                upButton.click();
-            }
-            wait.until(ExpectedConditions.elementToBeClickable(By.className("add-to-cart")));
-            webDriver.findElement(By.className("add-to-cart")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("quantity_wanted")));
+            WebElement input = webDriver.findElement(By.id("quantity_wanted"));
+            input.sendKeys(Keys.chord(this.controlKey, "A", Keys.DELETE));
+            input.sendKeys(String.valueOf(quantities[q]));
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.add-to-cart")));
+            webDriver.findElement(By.cssSelector("button.add-to-cart")).click();
             try {
                 Thread.sleep(Duration.ofSeconds(1));
             } catch (InterruptedException ex) {
             }
+            if (i < expectedResult + 1) webDriver.close();
         }
         webDriver.navigate().refresh();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-products-count")));
@@ -116,10 +105,12 @@ public class MainTest {
         assertEquals(((expectedResult + 1) * expectedResult) / 2, Integer.valueOf(result));
         return;
     }
+    @Order(2)
     @Test
     public void testSearchAndAddToCart() {
         webDriver.get("http://localhost:8089/en/");
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-products-count")));
+        String expectedResult = webDriver.findElement(By.className("cart-products-count")).getText().replaceAll("[()]", "");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ui-autocomplete-input")));
         WebElement searchBar = webDriver.findElement(By.className("ui-autocomplete-input"));
         searchBar.clear();
@@ -138,11 +129,112 @@ public class MainTest {
         webDriver.navigate().refresh();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-products-count")));
         String result = webDriver.findElement(By.className("cart-products-count")).getText().replaceAll("[()]", "");
-        assertEquals(1, Integer.valueOf(result));
+        assertEquals(Integer.parseInt(expectedResult) + 1, Integer.parseInt(result));
         return;
     }
+    @Order(3)
+    @Test
+    //TODO Yarnstreet asks to confirm the deletion of product through an alert window
     public void testRemoveFromCart() {
-
+        webDriver.get("http://localhost:8089/en/cart?action=show");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-products-count")));
+        String cartSize = webDriver.findElement(By.className("cart-products-count")).getText().replaceAll("[()]", "");
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("remove-from-cart")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("js-cart-line-product-quantity")));
+        int deleteButtonsSize = webDriver.findElements(By.className("remove-from-cart")).size();
+        int size = Math.min(deleteButtonsSize, 3);
+        int removed = 0;
+        for (int i = 0; i < size; i++) {
+            removed += Integer.parseInt(webDriver.findElement(By.className("js-cart-line-product-quantity")).getAttribute("value"));
+            webDriver.findElements(By.className("remove-from-cart")).get(0).click();
+            try {
+                Thread.sleep(Duration.ofSeconds(1));
+            } catch (InterruptedException ex) {
+            }
+        }
+        webDriver.navigate().refresh();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-products-count")));
+        String result = webDriver.findElement(By.className("cart-products-count")).getText().replaceAll("[()]", "");
+        assertEquals(Integer.parseInt(cartSize) - removed,  Integer.parseInt(result));
+        return;
+    }
+    @Order(4)
+    @Test
+    //TODO Yarnstreet has different fileds (email, password, confirm password)
+    public void testCreateAccount(){
+        webDriver.get("http://localhost:8089/en/login?create_account=1");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-firstname")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-lastname")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-email")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-password")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.form-control-submit")));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("customer_privacy")));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("psgdpr")));
+        webDriver.findElement(By.id("field-firstname")).sendKeys("UserA");
+        webDriver.findElement(By.id("field-lastname")).sendKeys("UserA");
+        webDriver.findElement(By.id("field-email")).sendKeys(UUID.randomUUID().toString() + "@user");
+        webDriver.findElement(By.id("field-password")).sendKeys("12345678");
+        webDriver.findElement(By.name("customer_privacy")).click();
+        webDriver.findElement(By.name("psgdpr")).click();
+        webDriver.findElement(By.cssSelector("button.form-control-submit")).click();
+        try {
+            Thread.sleep(Duration.ofSeconds(1));
+        } catch (InterruptedException ex) {
+        }
+        assertDoesNotThrow(() -> webDriver.findElement(By.className("account")));
+        return;
+    }
+    @Order(5)
+    @Test
+    //TODO change delivery and payment options' id
+    public void testPlaceOrder() {
+        webDriver.get("http://localhost:8089/en/cart?action=show");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.btn-primary")));
+        webDriver.findElement(By.cssSelector("a.btn-primary")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-address1")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-postcode")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("field-city")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[name='confirm-addresses']")));
+        webDriver.findElement(By.id("field-address1")).sendKeys("Adress");
+        webDriver.findElement(By.id("field-postcode")).sendKeys("11-111");
+        webDriver.findElement(By.id("field-city")).sendKeys("City");
+        webDriver.findElement(By.cssSelector("button[name='confirm-addresses']")).click();
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("delivery_option_1")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[name='confirmDeliveryOption']")));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) webDriver;
+        jsExecutor.executeScript("arguments[0].click();", webDriver.findElement(By.id("delivery_option_1")));
+        webDriver.findElement(By.cssSelector("button[name='confirmDeliveryOption']")).click();
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("payment-option-1")));
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("conditions_to_approve[terms-and-conditions]")));
+        webDriver.findElement(By.id("payment-option-1")).click();
+        webDriver.findElement(By.id("conditions_to_approve[terms-and-conditions]")).click();
+        try {
+            Thread.sleep(Duration.ofSeconds(1));
+        } catch (InterruptedException ex) {
+        }
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("//div[@id='payment-confirmation']//button[@type='submit']")));
+        webDriver.findElement(By.xpath("//div[@id='payment-confirmation']//button[@type='submit']")).click();
+        try {
+            Thread.sleep(Duration.ofSeconds(1));
+        } catch (InterruptedException ex) {
+        }
+        assertDoesNotThrow(() -> webDriver.findElement(By.className("card-block")));
+        return;
+    }
+    @Order(6)
+    @Test
+    public void testCheckProductDetails() {
+        webDriver.get("http://localhost:8089/en/my-account");
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("history-link")));
+        webDriver.findElement(By.id("history-link")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[data-link-action='view-order-details']")));
+        webDriver.findElement(By.cssSelector("a[data-link-action='view-order-details']")).click();
+        assertDoesNotThrow(() -> webDriver.findElement(By.id("order-history")));
+        return;
+    }
+    @Order(7)
+    @Test
+    public void testVATFacture() {
         return;
     }
 }
